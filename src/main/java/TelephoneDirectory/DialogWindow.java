e2512698d0c9e3f913645f859ec7f5d6ba81ecf0
AVKhakhalin <avkhakhalin@mail.ru>
2021-03-19 18:19:14 +0300

    Task1 well done.

--- src/main/java/TelephoneDirectory/DialogWindow.java
+++ src/main/java/TelephoneDirectory/DialogWindow.java
@@ -12,8 +12,10 @@ public class DialogWindow
     int sizeWidth;
     int sizeHeight;
 
-    JTextField[][] textField_1_;
-    String[][] arrayInputNumbers;
+    public final int NUMBER_WORDS = 20;
+    public final int NUMBER_WORDS_PER_ROW = 9;
+    public final String[] sampleWords = {"Абрикос", "Киви", "Арбуз", "Вишня", "Черешня", "Нектарин", "Айва", "Малина", "Арбуз", "Черешня", "Манго", "Абрикос", "Виноград", "Памело", "Мандарин", "Апельсин", "Банан", "Абрикос", "Абрикос", "Клубника"};
+    JTextField[] textField_1_;
 
     DialogWindow (int _sizeWidth, int _sizeHeight)
     {
@@ -31,8 +33,8 @@ public class DialogWindow
         // Добавляем текст в окно
         JLabel blueLabel = new JLabel("<html><font face=\"MyFont, Verdana, Arial\", size=\"5\"><br>" +
                 "Домашнее задание №10 включало в себя следующее:<br>" +
-                "1. Создать массив с набором слов (10-20 слов, должны встречаться повторяющиеся). Найти и вывести список уникальных слов, из которых состоит массив (дубликаты не считаем). Посчитать, сколько раз встречается каждое слово.<br><br><br><br>" +
-                "2. Написать простой класс Телефонный Справочник, который хранит в себе список фамилий и телефонных номеров. В этот телефонный справочник с помощью метода add() можно добавлять записи, а с помощью метода get() искать номер телефона по фамилии. Следует учесть, что под одной фамилией может быть несколько телефонов (в случае однофамильцев), тогда при запросе такой фамилии должны выводиться все телефоны. Желательно не добавлять лишний функционал (дополнительные поля (имя, отчество, адрес), взаимодействие с пользователем через консоль и т.д). Консоль использовать только для вывода результатов проверки телефонного справочника.</html>");
+                "1. Создать массив с набором слов (10-20 слов, должны встречаться повторяющиеся). Найти и вывести список уникальных слов, из которых состоит массив (дубликаты не считаем). Посчитать, сколько раз встречается каждое слово.<br><br><br><br><br>" +
+                "2. Написать простой класс Телефонный Справочник, который хранит в себе список фамилий и телефонных номеров. В этот телефонный справочник с помощью метода add()<br>можно добавлять записи, а с помощью метода get() искать номер телефона по фамилии. Следует учесть, что под одной фамилией может быть несколько телефонов (в случае однофамильцев), тогда при запросе такой фамилии должны выводиться все телефоны. Желательно не добавлять лишний функционал (дополнительные поля (имя, отчество, адрес), взаимодействие с пользователем через консоль и т.д). Консоль использовать только для вывода результатов проверки телефонного справочника.</html>");
         blueLabel.setLocation(30, 0); // координаты текста
         blueLabel.setSize(sizeWidth - 70, sizeHeight - 100); // размер области с текстом
         blueLabel.setVerticalAlignment(1);
@@ -43,11 +45,30 @@ public class DialogWindow
         // Создание кнопки для задания №1
         JButton task_1_button = new JButton("Проверить задание");
         task_1_button.setToolTipText("<html>Нажмите для проверки задания №1</html>");
-        task_1_button.setLocation(700, 103); // это координаты кнопки
+        task_1_button.setLocation(700, 123); // это координаты кнопки
         task_1_button.setSize(150,40 ); // это размер кнопки
         totalGUI.add(task_1_button);
         ActionListener actionListener1 = new TestActionListener(1); // Подключение обработчика событий к кнопке
         task_1_button.addActionListener(actionListener1);
+
+        // Создание текстовых полей для ввода слова для задания №1
+        textField_1_ = new JTextField[NUMBER_WORDS];
+        int tempDelta_x = 70;
+        int tempDelta_y = 30;
+        int counterRows = 0;
+        for (int i = 0; i < NUMBER_WORDS; i++)
+        {
+            textField_1_[i] = new JTextField(10);
+            textField_1_[i].setToolTipText("<html>Введите слово для задания №1 и нажмите соответствующую кнопку<br>\"Проверить задание\"</html>");
+            textField_1_[i].setFont(new Font(Font.SANS_SERIF,Font.PLAIN,16));
+            textField_1_[i].setBounds(60 + tempDelta_x * (i - counterRows * NUMBER_WORDS_PER_ROW), 123 + tempDelta_y * counterRows, 70, 30);
+            textField_1_[i].setText(sampleWords[i]);
+            totalGUI.add(textField_1_[i]);
+            if ((i + 1) % NUMBER_WORDS_PER_ROW == 0)
+            {
+                counterRows++;
+            }
+        }
 
         // Создание кнопки для задания №2
         JButton task_2_button = new JButton("Проверить задание");
@@ -86,7 +107,26 @@ public class DialogWindow
             switch (taskNumber)
             {
                 case 1:
-                    SolveTask1 solveTask1 = new SolveTask1();
+                    boolean existSomeWords = false;
+                    String[] newWords = new String[NUMBER_WORDS];
+                    for (int i = 0; i < textField_1_.length; i++)
+                    {
+                        if ((textField_1_[i].getText() != null) && (textField_1_[i].getText().length() > 0))
+                        {
+                            newWords[i] = textField_1_[i].getText();
+                            existSomeWords = true;
+                        }
+                    }
+                    if (existSomeWords == true)
+                    {
+                        SolveTask1 solveTask1 = new SolveTask1(newWords);
+                        solveTask1.getWordInfo();
+                    }
+                    else
+                    {
+                        System.out.println("Для проверки домашнего задания №1 нужно ввести в текстовые поля хотя бы одно слово.");
+                        JOptionPane.showMessageDialog(new JFrame(), "<html><font face=\"MyFont, Verdana, Arial\", size=\"4\">Для проверки домашнего задания №1<br>нужно ввести в текстовые поля хотя бы одно слово.<br>Пожалуйста, исправьте размер массива<br>и нажмите на кнопку \"Проверить задание\" ещё раз.</html>", "ОШИБКА", JOptionPane.ERROR_MESSAGE);
+                    }
                     break;
                 case 2:
                     SolveTask2 solveTask2 = new SolveTask2();
--- src/main/java/TelephoneDirectory/SolveTask1.java
+++ src/main/java/TelephoneDirectory/SolveTask1.java
@@ -1,5 +1,72 @@
 package TelephoneDirectory;
 
+import javax.swing.*;
+import java.util.Iterator;
+import java.util.LinkedHashSet;
+import java.util.LinkedList;
+import java.util.Set;
+
 public class SolveTask1
 {
+    Set<String> words = new LinkedHashSet<String>();
+    LinkedList<Integer> indexes = new LinkedList<Integer>();
+
+    SolveTask1(String[] newWords)
+    {
+        int curSize;
+        boolean foundSimilar;
+        int counter;
+        for (String analiseWord : newWords)
+        {
+            curSize = words.size();
+            words.add(analiseWord);
+            if (curSize == words.size())
+            {
+                Iterator<String> iterWords = words.iterator();
+                foundSimilar = false;
+                counter = 0;
+                while((iterWords.hasNext() == true) && (foundSimilar == false))
+                {
+                    if (iterWords.next().equals(analiseWord) == true)
+                    {
+                        indexes.set(counter, indexes.get(counter) + 1);
+                        foundSimilar = true;
+                    }
+                    counter++;
+                }
+            }
+            else
+            {
+                indexes.add(1);
+            }
+        }
+    }
+
+    public void getWordInfo()
+    {
+        int counter = 0;
+        Iterator<String> iterWords = words.iterator();
+        String resultsToSout = "Перечень уникальных слов (частота их присутствия в списке):\n";
+        String resultsToMessage = "Перечень уникальных слов (частота их присутствия в списке):<br>";
+        String tempString;
+        while (iterWords.hasNext() == true)
+        {
+            tempString = iterWords.next();
+            resultsToSout += (counter + 1) + ". " + tempString + " (" + indexes.get(counter) + ")";
+            resultsToMessage += (counter + 1) + ". " + tempString + " (" + indexes.get(counter) + ")";
+            if (iterWords.hasNext() == true)
+            {
+                resultsToSout += ".\n";
+                resultsToMessage += ".<br>";
+            }
+            else
+            {
+                resultsToSout += ".";
+                resultsToMessage += ".";
+            }
+            counter++;
+        }
+        System.out.println(resultsToSout);
+        JOptionPane.showMessageDialog(new JFrame(), "<html><font face=\"MyFont, Verdana, Arial\", size=\"4\">" + resultsToMessage + "</html>", "ИНФОРМАЦИЯ", JOptionPane.INFORMATION_MESSAGE);
+    }
 }
